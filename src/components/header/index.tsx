7707df97e7575838fe1b8db469ea4adfd5776e5a
DanielFariias <danielfariias15@gmail.com>
2023-08-18 14:28:45 -0300

    feat: :art: Modal new transaction created

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,8 @@
 import * as S from './styles'
 
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../new-transaction-modal'
 
 export function Header() {
   return (
@@ -8,7 +10,13 @@ export function Header() {
       <S.Content className="container">
         <img src={logoImg} alt="" />
 
-        <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </S.Content>
     </S.Wrapper>
   )
