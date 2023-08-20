31024094549894e751ef267d6b224f952df13b8e
DanielFariias <danielfariias15@gmail.com>
2023-08-20 14:51:54 -0300

    feat: :art: creating hook useSummary

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -1,39 +1,21 @@
+import { useTheme } from 'styled-components'
+
+import { useSummary } from '@/hooks/use-summary'
+
+import { currencyFormatter } from '@/utils/formmater'
+
 import {
   ArrowCircleDown,
   ArrowCircleUp,
   CurrencyCircleDollar,
 } from 'phosphor-react'
+
 import * as S from './styles'
-import { useTheme } from 'styled-components'
-import { TransactionsContext } from '@/contexts/transactions-context'
-import { useContext } from 'react'
-import { currencyFormatter } from '@/utils/formmater'
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext)
-
   const theme = useTheme()
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
-
-  console.log(summary)
+  const summary = useSummary()
 
   return (
     <S.Container className="container">
